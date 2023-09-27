fdfb1cc712de4ab2e411ddd58a5756ca30d11506
K-KY <kyuyoungk@naver.com>
2023-09-28 08:42:44 +0900

    fix : 에러메세지에 [ERROR] 추가

--- src/main/java/bridge/view/ErrorMessage.java
+++ src/main/java/bridge/view/ErrorMessage.java
@@ -1,7 +1,7 @@
 package bridge.view;
 
 public enum ErrorMessage {
-    ERROR_NOT_CORRECT("올바른 입력이 아닙니다");
+    ERROR_NOT_CORRECT("[ERROR] 올바른 입력이 아닙니다");
     private final String message;
 
     ErrorMessage(String message) {
