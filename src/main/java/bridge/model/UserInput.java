cebc3bcf77e7994b39bbb1c50f410dfb311cfeda
K-KY <kyuyoungk@naver.com>
2023-09-29 06:54:13 +0900

    Fix : inputLength 의 validateInput을 외부에서 주입받도록 변경

--- src/main/java/bridge/model/UserInput.java
+++ src/main/java/bridge/model/UserInput.java
@@ -4,8 +4,7 @@ import bridge.model.validator.ValidateInput;
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserInput {
-    public int inputLength() {
-        ValidateInput validateInput = new ValidateInput();
+    public int inputLength(ValidateInput validateInput) {
         String input = Console.readLine();
         while (!validateInput.validateLength(input)) {
             input = Console.readLine();
