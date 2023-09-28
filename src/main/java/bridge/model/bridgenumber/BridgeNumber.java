7d09ca6b74dbf480b57e2b7dce2be3c7ace903ea
K-KY <kyuyoungk@naver.com>
2023-09-29 06:56:02 +0900

    Feat : 생성된 숫자를 저장할 객체

---
+++ src/main/java/bridge/model/bridgenumber/BridgeNumber.java
@@ -0,0 +1,12 @@
+package bridge.model.bridgenumber;
+
+public class BridgeNumber {
+    private final int numbers;
+    public BridgeNumber(int numbers) {
+        this.numbers = numbers;
+    }
+
+    public int getNumbers() {
+        return numbers;
+    }
+}
