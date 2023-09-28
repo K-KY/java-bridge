6e9a5c3c283402cde477b498194d88ff3a35bdfa
K-KY <kyuyoungk@naver.com>
2023-09-28 10:01:06 +0900

    Feat : 길이를 입력받는 메소드

---
+++ src/main/java/bridge/model/UserInput.java
@@ -0,0 +1,15 @@
+package bridge.model;
+
+import bridge.model.validator.ValidateInput;
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserInput {
+    public int inputLength() {
+        ValidateInput validateInput = new ValidateInput();
+        String input = Console.readLine();
+        while (validateInput.validateLength(input)) {
+            input = Console.readLine();
+        }
+        return validateInput.integerParsing(input);
+    }
+}
