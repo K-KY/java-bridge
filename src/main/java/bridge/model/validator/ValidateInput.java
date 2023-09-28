df4e9d839e0b9c096b7d5e2cd2b08d83563eb333
K-KY <kyuyoungk@naver.com>
2023-09-28 09:58:57 +0900

    fix : vvalidateUserInput -> validateLength

--- src/main/java/bridge/model/validator/ValidateInput.java
+++ src/main/java/bridge/model/validator/ValidateInput.java
@@ -3,7 +3,8 @@ package bridge.model.validator;
 import bridge.view.ErrorMessage;
 
 public class ValidateInput {
-    public boolean validateUserInput(String userInput) {
+    public boolean validateLength(String userInput) {
+        //try 에서 예외가 나오지 않으면 false 를 리턴한다.
         try{
             integerParsing(userInput);
             return false;
@@ -13,11 +14,12 @@ public class ValidateInput {
         return true;
     }
 
-    private void integerParsing(String userInput) {
+    public int integerParsing(String userInput) {
         try {
             Integer.parseInt(userInput);
         } catch (Exception e) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_NOT_CORRECT.getMessage());
         }
+        return Integer.parseInt(userInput);
     }
 }
