1c511bac348f76811dbb8095170e8e4b513fab62
K-KY <kyuyoungk@naver.com>
2023-09-30 04:05:49 +0900

    Test : BridgeMap.class 메서드 테스트

--- src/test/java/bridge/MethodTest.java
+++ src/test/java/bridge/MethodTest.java
@@ -3,6 +3,7 @@ package bridge;
 import bridge.model.bridgenumber.BridgeMap;
 import bridge.model.bridgenumber.RandomBridgeNumber;
 import bridge.model.validator.ValidateInput;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,12 +12,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class MethodTest {
 
     @Test
+    @DisplayName("숫자가 아닌 값이 입력되면 false를 리턴한다.")
     void 잘못된_값이_입력되면_FALSE() {
         boolean b = new ValidateInput().validateLength("a");
         assertThat(b).isFalse();
     }
 
     @Test
+    @DisplayName("숫자가 아닌 값이 입력되면 예외를 발생시킨다.")
     void 잘못된_값이_입력되면_예외() {
         assertThatThrownBy(() -> new ValidateInput().integerParsing("a")).isInstanceOf(IllegalArgumentException.class);
     }
@@ -25,12 +28,14 @@ public class MethodTest {
 
 
     @Test
+    @DisplayName("입력된 숫자만큼 숫자를 생성한다.")
     void 숫자_생성_테스트() {
         RandomBridgeNumber randomBridgeNumber = new RandomBridgeNumber(3);
         BridgeMap list = randomBridgeNumber.bridgeNumberList();
         assertThat(list.size()).isEqualTo(3);
     }
     @Test
+    @DisplayName("숫자를 삭제한다.")
     void 숫자_삭제_테스트() {
         RandomBridgeNumber randomBridgeNumber = new RandomBridgeNumber(3);
         BridgeMap list = randomBridgeNumber.bridgeNumberList();
@@ -38,10 +43,15 @@ public class MethodTest {
         assertThat(list.size()).isEqualTo(2);
     }
     @Test
-    void 숫자_가져오기_테스트() {
+    @DisplayName("숫자를 가져오고 값이 0또는 1인지 확인한다.")
+    void 숫자_크기_테스트() {
         RandomBridgeNumber randomBridgeNumber = new RandomBridgeNumber(3);
         BridgeMap list = randomBridgeNumber.bridgeNumberList();
-        assertThat(list.find()).isGreaterThanOrEqualTo(0);
+        for (int i = 0; i < list.size(); i++) {
+            int number = list.delete().getNumbers();
+            assertThat(number).isIn(0,1);
+        }
+
     }
 
 
