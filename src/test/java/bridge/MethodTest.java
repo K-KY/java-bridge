15f2aed2a1e595974f9ff3869515b7a0b990fd86
K-KY <kyuyoungk@naver.com>
2023-09-30 03:49:22 +0900

    Test : BridgeMap.class 메서드 테스트

--- src/test/java/bridge/MethodTest.java
+++ src/test/java/bridge/MethodTest.java
@@ -1,16 +1,48 @@
 package bridge;
 
+import bridge.model.bridgenumber.BridgeMap;
+import bridge.model.bridgenumber.RandomBridgeNumber;
 import bridge.model.validator.ValidateInput;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class MethodTest {
 
     @Test
-    void 잘못된_값이_입력되면_True() {
+    void 잘못된_값이_입력되면_FALSE() {
         boolean b = new ValidateInput().validateLength("a");
-        assertThat(b).isTrue();
+        assertThat(b).isFalse();
     }
+
+    @Test
+    void 잘못된_값이_입력되면_예외() {
+        assertThatThrownBy(() -> new ValidateInput().integerParsing("a")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+
+
+    @Test
+    void 숫자_생성_테스트() {
+        RandomBridgeNumber randomBridgeNumber = new RandomBridgeNumber(3);
+        BridgeMap list = randomBridgeNumber.bridgeNumberList();
+        assertThat(list.size()).isEqualTo(3);
+    }
+    @Test
+    void 숫자_삭제_테스트() {
+        RandomBridgeNumber randomBridgeNumber = new RandomBridgeNumber(3);
+        BridgeMap list = randomBridgeNumber.bridgeNumberList();
+        list.delete();
+        assertThat(list.size()).isEqualTo(2);
+    }
+    @Test
+    void 숫자_가져오기_테스트() {
+        RandomBridgeNumber randomBridgeNumber = new RandomBridgeNumber(3);
+        BridgeMap list = randomBridgeNumber.bridgeNumberList();
+        assertThat(list.find()).isGreaterThanOrEqualTo(0);
+    }
+
 
 }
