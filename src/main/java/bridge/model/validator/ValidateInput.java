aa7a10e353879b595c2a3047d2e2a10f1e19b691
K-KY <kyuyoungk@naver.com>
2023-10-01 02:53:05 +0900

    fix : 입력된 숫자 <= 0 이라면 예외

--- src/main/java/bridge/model/validator/ValidateInput.java
+++ src/main/java/bridge/model/validator/ValidateInput.java
@@ -3,10 +3,10 @@ package bridge.model.validator;
 import bridge.view.ErrorMessage;
 
 public class ValidateInput {
-    public boolean validateLength(String userInput) {
+    public boolean validateLength(String bridgeLength) {
         //try 에서 예외가 나오지 않으면 false 를 리턴한다.
         try{
-            int n = integerParsing(userInput);
+            int n = integerParsing(bridgeLength);
             return validateRange(n);
         }catch (Exception e) {
             System.out.println(e.getMessage());
@@ -15,15 +15,19 @@ public class ValidateInput {
     }
 
     private boolean validateRange(int n) {
-        return n > 0;
+        if (n <= 0) {
+            throw new IllegalArgumentException(ErrorMessage.ERROR_NOT_CORRECT.getMessage());
+        }
+        return true;
     }
 
-    public int integerParsing(String userInput) {
+    public int integerParsing(String bridgeLength) {
         try {
-            Integer.parseInt(userInput);
+            Integer.parseInt(bridgeLength);
         } catch (Exception e) {
             throw new IllegalArgumentException(ErrorMessage.ERROR_NOT_CORRECT.getMessage());
         }
-        return Integer.parseInt(userInput);
+        return Integer.parseInt(bridgeLength);
     }
+
 }
